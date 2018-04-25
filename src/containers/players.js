da7e136804334f6ddb843236d3631519d02475aa
Dexter Gramfors <dexter.gramfors@trioptima.com>
2018-04-25 19:31:50 +0200

    Add basic player form.

--- src/containers/players.js
+++ src/containers/players.js
@@ -1,9 +1,36 @@
 import React from 'react'
 
+class AddPlayerForm extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {name: ''}
+  }
+
+  handleChange = (event) => {
+    this.setState({name: event.target.value})
+  }
+
+  addPlayer = (event) => {
+    console.log(this.state)
+    this.setState({name: ''})
+  }
+
+  render() {
+    return (
+      <div>
+        <div>Add player</div>
+        <input onChange={this.handleChange} value={this.state.name} className="player-input"/>
+        <button onClick={this.addPlayer}>Add</button>
+      </div>
+    )
+  }
+}
+
 const Players = () => (
   <div>
     <h1>Manage players</h1>
-    <p>Hello TriOptima!</p>
+    <AddPlayerForm/>
   </div>
 )
 
