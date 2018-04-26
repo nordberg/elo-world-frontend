0576118e3722591a0d75977e4c84ce4fc6517aef
Dexter Gramfors <dexter.gramfors@trioptima.com>
2018-04-26 13:15:04 +0200

    Add basic ranking component and placeholders for table tennis and 8-ball.

--- src/containers/home.js
+++ src/containers/home.js
@@ -1,8 +1,13 @@
 import React from 'react'
+import Ranking from 'containers/ranking'
 
 const Home = () => (
   <div>
     <h1>elo world</h1>
+    <div className="rankings">
+      <Ranking sport="8ball"/>
+      <Ranking sport="tableTennis"/>
+    </div>
   </div>
 )
 
